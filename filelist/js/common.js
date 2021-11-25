6b017669b9ab3beb38aacba56c5cf3cb7e44bb04
shjung84 <seunghyun.jung.84@gmail.com>
2021-11-25 17:18:04 +0900

    역학조사관 - 확진자관리 - 파일리스트

--- filelist/js/common.js
+++ filelist/js/common.js
@@ -4,16 +4,16 @@ $(function(){
 
 function layout(){
 	// Tab
-	$(".tab-btn button").on("click",function(){
+	var btn = $(".tab-btn button");
+	var cnt = $(".tab-content");
+	var on = $(".tab-btn .on");
+	var i = on.index()+1;
+	cnt.load("tab-content-0"+i+".html");
+	btn.on("click",function(){
 		var idx = $(this).index();
-		$(".tab-btn button").eq(idx).addClass("on").siblings().removeClass("on");
-		$(".tab-content").eq(idx).addClass("on").siblings().removeClass("on");
-	});
-
-	$(".tab-content").each(function(){
-		var box = $(this);
-		var idx = box.attr('aria-content');
-		box.load("tab-content-"+idx+".html");
+		var _idx = idx+1;
+		btn.eq(idx).addClass("on").siblings().removeClass("on");
+		cnt.load("tab-content-0"+_idx+".html");
 	});
 }
 
@@ -38,12 +38,55 @@ function state(){
 	$(".step-01").closest('tr').addClass("step-01");	//컨펌전
 	$(".step-02").closest('tr').addClass("step-02");	//진행중
 	$(".complete").closest('tr').addClass("complete");	//완료
+	$(".new").closest('tr').addClass("new");	//완료
 	$(".modify").closest('tr').addClass("modify");		//수정중
-	$(".new").closest('tr').addClass("new");			//업뎃
+	$(".update").closest('tr').addClass("update");			//업뎃
+}
+
+/* POPUP */
+function modalPopup(fileurl) {
+	$("body").addClass("modal");
+	$("body").append($("<div class='trplayer'></div>"));
+	$("body").append($("<div class='trplayerw'></div>"));
+
+	$(".trplayerw").append($("<div class='pop__modal'></div>"));
+	$(".pop__modal").load(fileurl, function(){
+		var modalpopWidth = $(".pop__modal").width();
+		$(".pop__modal").css("margin-left",-modalpopWidth/2+"px");
+		var modalpopHeight = $(".pop__modal").height();
+		$(".pop__modal").css("margin-top",-modalpopHeight/2+"px");
+		var windowHeight = $(window).height();
+		var windowWidth = $(window).width();
+		function modalCssChange(width, height) {
+			var width = parseInt(width);
+			var height = parseInt(height);
+			if (height < modalpopHeight){
+				$("body").addClass("a-height");
+			} else {
+				$("body").removeClass("a-height");
+			}
+			if (width < modalpopWidth){
+				$("body").addClass("a-width");
+			} else {
+				$("body").removeClass("a-width");
+			}
+		}
+		$(function() {
+			$(window).resize(function() {
+				modalCssChange($(this).width(), $(this).height());
+			}).resize();
+		});
+	});
+}
+function modalHide() {
+	$("body").removeClass("modal");
+	$(".trplayer").remove();
+	$(".trplayerw").remove();
+	$(".pop__modal").remove();
 }
 
 function reload(){
-	progress()
+	progress();
 	target();
-	state()
+	state();
 }
