b923ba878ffd6b73d0d31e89f54145413f655a3e
shjung84 <seunghyun.jung.84@gmail.com>
2021-12-03 20:38:17 +0900

    자가진단 샘플웹 > 완료

--- js/main.js
+++ js/main.js
@@ -1,24 +1,45 @@
 $(function(){
-	console.log(`START MAIN`);
-
 	maibGnb();		// GNB
+	agreechk()		// Sgin Up Checked
 });
-
 
 function maibGnb(){
 	var btn = $(".btn__gnb");
 	var gnb = $(".gnb");
-	var wrapper = $(".wrapper");
+	var close = $(".btn__gnb__close");
 	btn.on("click",function(){
-		if(wrapper.is(".gnb--active")){
-			wrapper.removeClass("gnb--active");
-		}else{
-			wrapper.addClass("gnb--active");
+		if(!gnb.is(".gnb--active")){
+			gnb.addClass("gnb--active");
+			gnb.animate({"left":"0"},300, function(){
+			})
+		}
+	});
+	close.on("click",function(){
+		if(gnb.is(".gnb--active")){
+			gnb.animate({"left":"-100%"},300, function(){
+				gnb.removeClass("gnb--active");
+			})
 		}
 	});
 }
 
-
+function agreechk(){
+	var chk = $(".input__chk-02 input");
+	$.each(chk,function(){
+		if($(this).is(":checked")){
+			$(this).closest("dd").attr("aria-agree-checked",true);
+		}else{
+			$(this).closest("dd").attr("aria-agree-checked",false);
+		}
+	});
+	chk.on("change",function(){
+		if($(this).is(":checked")){
+			$(this).closest("dd").attr("aria-agree-checked",true);
+		}else{
+			$(this).closest("dd").attr("aria-agree-checked",false);
+		}
+	});
+}
 
 
 
